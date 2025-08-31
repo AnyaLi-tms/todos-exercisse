b97be53b939ccf0cfd66e3eaa80f0dd5f24bbd65
thatsShirleyLee <2690233316@qq.com>
2025-08-31 14:58:51 +0800

    feat: add main files

---
+++ src/test/java/com/oocl/todos/TodosExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
